64522bb69b384141f3270b1fce3fb4ce8824a278
allankevinscain <meuemail44allan@gmail.com>
2025-09-15 20:06:28 -0300

    chore: vercel adjustments

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.use(express.json());
 
 app.use("/api/characters", characterRoutes);
 
+app.use("/", (_, res) => {
+  res.send({ message: "🚀 ~ API harry potter está em órbita!" });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running at http://localhost:${process.env.PORT}`);
 });
